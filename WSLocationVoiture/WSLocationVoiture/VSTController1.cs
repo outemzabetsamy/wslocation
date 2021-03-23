7c496383c161a9775d918f8a2e6fada8cb621879
YOUCEF <samy.spy00006@gmail.com>
2021-03-23 01:52:06 +0100

    first commit

---
+++ WSLocationVoiture/WSLocationVoiture/VSTController1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WSLocationVoiture
+{
+    public class VSTController1 : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<station_taxi> GetStationTaxi()
+        {
+            location_dbEntitie entities = new location_dbEntitie();
+            return entities.station_taxi;
+        }
+
+        public IEnumerable<voiture> GetAgenceLocation()
+        {
+            location_dbEntitie entities = new location_dbEntitie();
+            return entities.voitures;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WSLocationVoiture/WSLocationVoiture/WebService1.asmx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WSLocationVoiture
+{
+    /*
+     INSERT INTO voiture(id, name, adresse, phoneNumber, email, image) 
+     select 2, 'SamirLoc','23 city seghir Bejaia', '0540121065', 'samirelocàgmail.com',BulkColumn from openrowset
+	 (bulk 'C:\Users\limitless\Desktop\MyStore-icnBlanc.png', SINGLE_BLOB) as image
+    */
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<voiture> GetAgenceLocation()
+        {
+            VSTController1 vc = new VSTController1();
+            return vc.GetAgenceLocation().ToList();
+        }
+
+        [WebMethod]
+        public List<station_taxi> GetStationTaxi()
+        {
+            VSTController1 vc = new VSTController1();
+            return vc.GetStationTaxi().ToList();
+        }
+    }
+}
